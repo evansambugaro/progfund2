ef6832c63483624c46c32169e3477656b071fa33
evansambugaro <93528067+evansambugaro@users.noreply.github.com>
2022-05-18 10:01:03 -0500

    Add files via upload

---
+++ cweb1113/project2/config/core.php
@@ -0,0 +1,16 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+
+// set your default time-zone
+date_default_timezone_set('America/Los_Angeles');
+
+// page is the current page, if there's nothing set, default is page 1
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+// set records or rows of data per page
+$records_per_page = 5;
+
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+?>
